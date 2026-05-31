bf2de69bbd070630416902d8e426201f04313c89
murphyphonethrowaway-bit <murphyphonethrowaway@gmail.com>
2026-05-31 09:13:33 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # ==================== GMAIL CONFIG ====================
 GMAIL_USER = "relicregistrybot@gmail.com"
-GMAIL_APP_PASSWORD = "vcwnbupjxqddpdkc"
+GMAIL_APP_PASSWORD = "hxurgoktwbeznvls"
 
 # ==================== DEALER CONFIG ====================
 # Web scraped dealers (still working)
